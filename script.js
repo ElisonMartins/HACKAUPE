bc17e75ee61b0495fe0fba38c0149f4ba9187f58
Elison Martins <68920728+ElisonMartins@users.noreply.github.com>
2022-08-17 21:20:40 -0300

    2.0

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function typeWriter(elemento) {
     }
 const titulo = document.querySelector('.title');
 typeWriter(titulo);
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('#head1', { delay: 200 });
+ScrollReveal().reveal('#head2', { delay: 200 });
+ScrollReveal().reveal('#head3', { delay: 200 });
+ScrollReveal().reveal('#head4', { delay: 200 });
